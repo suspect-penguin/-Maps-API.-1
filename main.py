f6b3c536dac88301d402c7148442379ee06e8c91
Borus25 <104708239+Borus25@users.noreply.github.com>
2023-01-30 17:58:01 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,51 +1,46 @@
-import pygame, requests, sys, os
+import os
+import sys
+
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from untitled import Ui_Form
 
 
-
-class MapParams(object):
+class MapWindow(QWidget, Ui_Form):
     def __init__(self):
-        self.x = 55
-        self.y = 83
-        self.zoom = 10
-        self.type = "map"
+        super().__init__()
+        self.setupUi(self)
+        self.initUI()
 
-    def ll(self):
-        return str(self.y) + "," + str(self.x)
+    def initUI(self):
+        self.setWindowTitle('Отображение карты')
+        map_request = "http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map"
+        response = requests.get(map_request)
 
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
 
-def load_map(mp):
-    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
-    response = requests.get(map_request)
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print(map_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    map_file = "map.png"
-    try:
-        with open(map_file, "wb") as file:
+        # Запишем полученное изображение в файл.
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
             file.write(response.content)
-    except IOError as ex:
-        print("Ошибка записи временного файла:", ex)
-        sys.exit(2)
-    return map_file
+
+        ## Изображение
+        self.pixmap = QPixmap(self.map_file)
+        self.image = QLabel(self)
+        self.image.setPixmap(self.pixmap)
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((600, 400))
-    mp = MapParams()
-    while True:
-        event = pygame.event.wait()
-        if event.type == pygame.QUIT:
-            break
-        map_file = load_map(mp)
-        screen.blit(pygame.image.load(map_file), (0, 0))
-        pygame.display.flip()
-    pygame.quit()
-    os.remove(map_file)
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MapWindow()
+    ex.show()
+    sys.exit(app.exec())
